af1241818cd53f84de1d40f52ce3ea66983f4cb5
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2024-11-16 02:31:08 +0200

    Add files via upload

---
+++ 2nd Year/1st Semester/Metode Avansate de Programare/laborator 3/src/domain/validators/ValidatorUser.java
@@ -0,0 +1,22 @@
+package domain.validators;
+
+import domain.User;
+
+public class ValidatorUser implements Validator<User> {
+
+    @Override
+    public void validate(User entity) throws ValidationException {
+        String errormsg = "";
+
+        if (entity.getFirstName().isEmpty())
+            errormsg += "Prenumele nu poate fi null";
+
+        if (entity.getLastName().isEmpty())
+            errormsg += "Numele de familie nu poate fi null";
+
+        System.out.println(errormsg);
+
+        if (!errormsg.isEmpty())
+            throw new ValidationException(errormsg);
+    }
+}
