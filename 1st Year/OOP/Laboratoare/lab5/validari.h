2caa16455412787b779fd4297b0711971d2455ca
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2024-11-16 02:05:26 +0200

    Add files via upload

---
+++ 1st Year/OOP/Laboratoare/lab5/validari.h
@@ -0,0 +1,11 @@
+//
+// Created by Emi on 3/23/2024.
+//
+
+#ifndef OOPLAB2_4_VALIDARI_H
+#define OOPLAB2_4_VALIDARI_H
+
+int validare(int cantitate, char *nume, char *tip);
+
+
+#endif //OOPLAB2_4_VALIDARI_H
