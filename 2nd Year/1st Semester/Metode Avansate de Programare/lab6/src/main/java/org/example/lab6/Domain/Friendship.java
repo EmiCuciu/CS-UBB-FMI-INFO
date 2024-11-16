75b7e33c87c2dd4d25a3b9af7d75a0ea49596e5f
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2024-11-16 04:14:12 +0200

    commit-uri

--- 2nd Year/1st Semester/Metode Avansate de Programare/lab6/src/main/java/org/example/lab6/Domain/Friendship.java
+++ 2nd Year/1st Semester/Metode Avansate de Programare/lab6/src/main/java/org/example/lab6/Domain/Friendship.java
@@ -1,4 +1,43 @@
 package org.example.lab6.Domain;
 
-public class Friendship {
+public class Friendship extends Entity<Tuple<Long,Long>>{
+
+        public Friendship(Long id1, Long id2) {
+            setId(new Tuple<>(id1, id2));
+        }
+
+        public Friendship(Tuple<Long, Long> id) {
+            setId(id);
+        }
+
+        public Long getId1() {
+            return getId().getLeft();
+        }
+
+        public Long getId2() {
+            return getId().getRight();
+        }
+
+        public void setId1(Long id1) {
+            setId(new Tuple<>(id1, getId2()));
+        }
+
+        public void setId2(Long id2) {
+            setId(new Tuple<>(getId1(), id2));
+        }
+
+        @Override
+        public String toString() {
+            return "Prietenie intre: " + getId1() + " si " + getId2();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return this.getId().equals(((Friendship) obj).getId());
+        }
+
+        @Override
+        public int hashCode() {
+            return getId().hashCode();
+        }
 }
