3858af2c7e19e32929a9a6e29a005d08cfd3cd2e
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2024-11-16 02:31:08 +0200

    Add files via upload

---
+++ 2nd Year/1st Semester/Metode Avansate de Programare/laborator 3/src/domain/validators/ValidatorPrietenie.java
@@ -0,0 +1,22 @@
+package domain.validators;
+
+import domain.Prietenie;
+
+import java.util.Objects;
+
+public class ValidatorPrietenie implements Validator<Prietenie> {
+
+    @Override
+    public void validate(Prietenie entity) throws ValidationException {
+
+        String errors = "";
+
+        if (Objects.equals(entity.getIdUser1(), entity.getIdUser2()))
+            errors += "TREBUIE PERSOANE DIFERITE\n";
+
+        System.out.println(errors);
+
+        if (!errors.isEmpty())
+            throw new ValidationException(errors);
+    }
+}
