6425ed54e13e97c38e2a6ea428fc0277489f7968
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2024-11-16 04:57:32 +0200

    e ca a lu profa

--- 2nd Year/1st Semester/Metode Avansate de Programare/lab6/src/main/java/org/example/lab6/HelloController.java
+++ 2nd Year/1st Semester/Metode Avansate de Programare/lab6/src/main/java/org/example/lab6/HelloController.java
@@ -1,14 +1,32 @@
 package org.example.lab6;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import org.example.lab6.Domain.User;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    private Button btnHello;
+
+    @FXML
+    ListView<User> listView;
+
+    @FXML
+    public void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+        btnHello.setText("alt text");
+        List<User> list = Arrays.asList(new User("dan", "ana"));
+        ObservableList<User> observableList = FXCollections.observableList(list);
+        listView.setItems(observableList);
     }
 }
