e6260059bce96cbd8d6a1e73b7de846dddfc7821
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2024-11-16 04:02:30 +0200

    am adaugat User

--- 2nd Year/1st Semester/Metode Avansate de Programare/lab6/src/main/java/org/example/lab6/Domain/User.java
+++ 2nd Year/1st Semester/Metode Avansate de Programare/lab6/src/main/java/org/example/lab6/Domain/User.java
@@ -1,4 +1,72 @@
 package org.example.lab6.Domain;
 
-public class User {
+import java.util.Objects;
+
+public class User extends Entity<Long> {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstname='" + firstName + '\'' +
+                ", lastname='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        User user = (User) o;
+        return Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), firstName, getLastName());
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
 }
