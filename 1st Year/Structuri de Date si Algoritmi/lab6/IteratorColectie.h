5f5d98343f01e3f8ca00938095d29ca40cc0ce92
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2024-11-16 02:18:43 +0200

    Add files via upload

---
+++ 1st Year/Structuri de Date si Algoritmi/lab6/IteratorColectie.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Colectie.h"
+
+class Colectie;
+typedef int TElem;
+
+class IteratorColectie
+{
+	friend class Colectie;
+private:
+	//constructorul primeste o referinta catre Container
+    //iteratorul va referi primul element din container
+	IteratorColectie(const Colectie& c);
+
+    void deplasare();
+    //contine o referinta catre containerul pe care il itereaza
+	const Colectie& col;
+	/* aici e reprezentarea pecifica a iteratorului*/
+    int pozCurent;
+    Node* curent;
+    int frecvCurenta;
+
+public:
+
+		//reseteaza pozitia iteratorului la inceputul containerului
+		void prim();
+
+		//muta iteratorul in container
+		// arunca exceptie daca iteratorul nu e valid
+		void urmator();
+
+		//verifica daca iteratorul e valid (indica un element al containerului)
+		bool valid() const;
+
+		//returneaza valoarea elementului din container referit de iterator
+		//arunca exceptie daca iteratorul nu e valid
+		TElem element() const;
+
+};
+
