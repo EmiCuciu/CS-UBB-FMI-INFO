cbb45e65db390962e0e9c20ce12d3f1a0d124614
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2024-11-17 22:39:12 +0200

    gata

---
+++ 2nd Year/1st Semester/Metode Avansate de Programare/lab6/src/main/java/com/example/guiex1/domain/Prietenie.java
@@ -0,0 +1,38 @@
+package com.example.guiex1.domain;
+
+import java.time.LocalDateTime;
+
+public class Prietenie {
+    private Tuple<Long, Long> id;
+    private LocalDateTime date;
+    private String status;
+
+    public Prietenie(LocalDateTime date, String status) {
+        this.date = date;
+        this.status = status;
+    }
+
+    public Tuple<Long, Long> getId() {
+        return id;
+    }
+
+    public void setId(Tuple<Long, Long> id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
