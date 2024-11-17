b93400fa44c627abf45c268a5b35429934166732
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2024-11-17 22:40:05 +0200

    Delete 2nd Year/1st Semester/Metode Avansate de Programare/lab6 directory

--- 2nd Year/1st Semester/Metode Avansate de Programare/lab6/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.lab6 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.lab6 to javafx.fxml;
-    exports org.example.lab6;
-}
