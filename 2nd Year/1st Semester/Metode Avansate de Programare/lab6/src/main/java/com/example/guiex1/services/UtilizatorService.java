52d60aa6bc905ee27abdafcb7fc47a676a38f469
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2024-11-18 14:53:41 +0200

    @

---
+++ 2nd Year/1st Semester/Metode Avansate de Programare/lab6/src/main/java/com/example/guiex1/services/UtilizatorService.java
@@ -0,0 +1,104 @@
+package com.example.guiex1.services;
+
+import com.example.guiex1.domain.Prietenie;
+import com.example.guiex1.domain.Utilizator;
+import com.example.guiex1.repository.Repository;
+import com.example.guiex1.repository.dbrepo.UtilizatorDbRepository;
+import com.example.guiex1.utils.events.ChangeEventType;
+import com.example.guiex1.utils.events.UtilizatorEntityChangeEvent;
+import com.example.guiex1.utils.observer.Observable;
+import com.example.guiex1.utils.observer.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public class UtilizatorService implements Observable<UtilizatorEntityChangeEvent> {
+    private Repository<Long, Utilizator> repo;
+    private List<Observer<UtilizatorEntityChangeEvent>> observers = new ArrayList<>();
+
+    public UtilizatorService(Repository<Long, Utilizator> repo) {
+        this.repo = repo;
+    }
+
+    public Utilizator addUtilizator(Utilizator user) {
+        if (repo.save(user).isEmpty()) {
+            UtilizatorEntityChangeEvent event = new UtilizatorEntityChangeEvent(ChangeEventType.ADD, user);
+            notifyObservers(event);
+            return null;
+        }
+        return user;
+    }
+
+    public Utilizator deleteUtilizator(Long id) {
+        Optional<Utilizator> user = repo.delete(id);
+        if (user.isPresent()) {
+            notifyObservers(new UtilizatorEntityChangeEvent(ChangeEventType.DELETE, user.get()));
+            return user.get();
+        }
+        return null;
+    }
+
+    public Iterable<Utilizator> getAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public void addObserver(Observer<UtilizatorEntityChangeEvent> e) {
+        observers.add(e);
+    }
+
+    @Override
+    public void removeObserver(Observer<UtilizatorEntityChangeEvent> e) {
+        observers.remove(e);
+    }
+
+    @Override
+    public void notifyObservers(UtilizatorEntityChangeEvent t) {
+        observers.forEach(x -> x.update(t));
+    }
+
+    public Utilizator updateUtilizator(Utilizator u) {
+        Optional<Utilizator> oldUser = repo.findOne(u.getId());
+        if (oldUser.isPresent()) {
+            Optional<Utilizator> newUser = repo.update(u);
+            if (newUser.isEmpty())
+                notifyObservers(new UtilizatorEntityChangeEvent(ChangeEventType.UPDATE, u, oldUser.get()));
+            return newUser.orElse(null);
+        }
+        return oldUser.orElse(null);
+    }
+
+    public void addFriend(Long userId, Long friendId) {
+        ((UtilizatorDbRepository) repo).addFriendRequest(userId, friendId);
+    }
+
+    public Set<Utilizator> findFriends(Long userId) {
+        return ((UtilizatorDbRepository) repo).findFriends(userId);
+    }
+
+    public void addFriendRequest(Long userId, Long friendId) {
+        ((UtilizatorDbRepository) repo).addFriendRequest(userId, friendId);
+    }
+
+    public void acceptFriendRequest(Long userId, Long friendId) {
+        ((UtilizatorDbRepository) repo).acceptFriendRequest(userId, friendId);
+    }
+
+    public void removeFriend(Long userId, Long friendId) {
+        ((UtilizatorDbRepository) repo).removeFriend(userId, friendId);
+    }
+
+    public Set<Prietenie> findFriendRequests(Long userId) {
+        return ((UtilizatorDbRepository) repo).findFriendRequests(userId);
+    }
+
+    public Optional<Utilizator> findByUsernameAndPassword(String username, String password) {
+        return ((UtilizatorDbRepository) repo).findByUsernameAndPassword(username, password);
+    }
+
+    public void saveUser(Utilizator user) {
+        ((UtilizatorDbRepository) repo).saveUser(user);
+    }
+}
