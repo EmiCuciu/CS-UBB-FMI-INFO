fd002ed1b027c290c306b58d735f7c744e8b2bc6
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2024-11-29 08:54:53 +0200

    trebuie sa fac TODO, lab de nota 9 momentan

--- 2nd Year/1st Semester/Metode Avansate de Programare/lab8/src/main/java/org/example/lab8/Main.java
+++ 2nd Year/1st Semester/Metode Avansate de Programare/lab8/src/main/java/org/example/lab8/Main.java
@@ -24,3 +24,17 @@ public class Main extends Application {
         launch(args);
     }
 }
+
+/**
+ * TODO: 1. Sa pot da reply la un mesaj, adica sa schimb areatext din messages in ceva tabela
+ *          , unde sa pot da click pe un mesaj si sa am un buton reply
+ *          care sa refere acel mesaj, de exemplu replyed to: "mesajul la care se raspunde"
+ *          , si sa pot scrie un mesaj nou care sa fie reply la acel mesaj
+        2. Sa imi fac observer pentru fiecare db, userdb,friendshipdb, messagedb, unde persistenta datelor sa fie adevarata
+        3. Sa fac un sistem de notificari, cand cineva iti trimite un mesaj, sa iti apara o notificare
+            adica sa nu fac manareli cu acel buton, sa fie un sistem de notificari
+        4. Sa fac un sistem de notificari pentru cand cineva iti trimite un mesaj, sa iti apara un badge(un sclipitor ceva) pe butonul de messages
+        5. Sa fac UN OBSERVER PENTRU TOT, adica sa schimb SceneManager din login in main, vreau ca fereastra login sa ramana mereu activa, de acolo sa ma pot conecta cu oricati useri vreau
+            scopul final sa am un login, iar celealte ferestre main sa aiba datele persistente pentru fiecare user logat
+
+ */
