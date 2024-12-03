eda0c9a692c6ba0ce02177d4210e97fb4e70379e
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2024-12-03 11:08:30 +0200

    seminar 10

---
+++ 2nd Year/1st Semester/Metode Avansate de Programare/seminar10 C#/seminar10/seminar10/Main.cs
@@ -0,0 +1,10 @@
+﻿namespace seminar10;
+
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Task task = new Task("1", "Descrierea task-ului");
+        Console.WriteLine(task.ToString());
+    }
+}
