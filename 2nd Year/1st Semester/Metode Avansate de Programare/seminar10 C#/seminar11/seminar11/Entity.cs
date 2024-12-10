69ee2fbca5a8b28adf05e1913b9b874bced27d6f
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2024-12-10 11:20:23 +0200

    seminar 11

---
+++ 2nd Year/1st Semester/Metode Avansate de Programare/seminar10 C#/seminar11/seminar11/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace seminar11;
+
+public class Entity<TID>
+{
+    public TID ID
+    {
+        get;
+        set;
+    }
+}
---
+++ 2nd Year/1st Semester/Metode Avansate de Programare/seminar10 C#/seminar11/seminar11/InMemoryRepository.cs
@@ -0,0 +1,58 @@
+﻿namespace seminar11;
+
+public class InMemoryRepository<ID, E> : IRepository<ID, E> where E : Entity<ID>
+{
+    protected IValidator<E> vali;
+    protected IDictionary<ID, E> entities = new Dictionary<ID, E>();
+
+    public InMemoryRepository(IValidator<E> vali)
+    {
+        this.vali = vali;
+        
+    }
+
+    public E FindOne(ID id)
+    {
+        if (entities.ContainsKey(id)) return entities[id];
+        return default(E);
+    }
+
+    public IEnumerable<E> FindAll()
+    {
+        return entities.Values;
+    }
+
+    public E Save(E entity)
+    {
+        if (entity == null) throw new ArgumentNullException("entity must not be null");
+        this.vali.Validate(entity);
+        if (this.entities.ContainsKey(entity.ID))
+        {
+            return entity;
+        }
+
+        this.entities[entity.ID] = entity;
+        return default(E);
+    }
+
+    public E Delete(ID id)
+    {
+        E entity = this.FindOne(id);
+        entities.Remove(id);
+        return entity;
+    }
+
+    public E Update(E entity)
+    {
+        if (entity == null) throw new ArgumentNullException("entity must not be null");
+        if (FindOne(entity.ID) == null)
+        {
+            return Save(entity);
+        }
+        else
+        {
+            entities[entity.ID] = entity;
+            return default(E);
+        }
+    }
+}
