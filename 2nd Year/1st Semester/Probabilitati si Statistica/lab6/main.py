910e0907b8daacaa1b934a65e3d07f12b0750f84
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2024-12-13 10:58:36 +0200

    lab11

--- 2nd Year/1st Semester/Probabilitati si Statistica/lab6/main.py
+++ 2nd Year/1st Semester/Probabilitati si Statistica/lab6/main.py
@@ -2,4 +2,6 @@ from scipy.stats import norm
 from numpy import mean, std, linspace
 from matplotlib.pyplot import plot, show,hist,grid,legend,xticks,plot
 
-data = norm.rvs(loc=0, scale=1, size=1000)
+n = 5000
+
+data = norm.rvs(loc=165, scale=1, size=1000)
