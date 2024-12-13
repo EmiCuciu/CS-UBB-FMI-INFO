7464d5d26a78265002eda01d116ff8464fa31a96
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2024-12-13 10:55:38 +0200

    lab11

--- 2nd Year/1st Semester/Probabilitati si Statistica/lab6/main.py
+++ 2nd Year/1st Semester/Probabilitati si Statistica/lab6/main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+from scipy.stats import norm
+from numpy import mean, std, linspace
+from matplotlib.pyplot import plot, show,hist,grid,legend,xticks,plot
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+data = norm.rvs(loc=0, scale=1, size=1000)
