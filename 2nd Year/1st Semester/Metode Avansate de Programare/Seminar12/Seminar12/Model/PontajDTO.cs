a3f3de905de2961a2d55aed9a566ecb4bc8ffa0d
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2025-01-09 13:30:52 +0200

    laburi

---
+++ 2nd Year/1st Semester/Metode Avansate de Programare/Seminar12/Seminar12/Model/PontajDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar12.Model
+{
+    class PontajDTO
+    {
+        public String NumeAngajat { get; set; }
+        public Double Salar { get; set; }
+
+        public KnowledgeLevel Nivel { get; set; }
+
+        public override string ToString()
+        {
+            return $"{NumeAngajat} {Salar} {Nivel.ToString()}"; // incepand cu C#6
+        }
+    }
+}
---
+++ 2nd Year/1st Semester/Metode Avansate de Programare/Seminar12/Seminar12/Model/Validator/PontajValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar12.Model{
+    class PontajValidator : IValidator<Pontaj>
+    {
+        public void Validate(Pontaj e)
+        {
+            bool valid = true;
+            if (valid == false)
+            {
+                throw new ValidationException("Obiectul nu e valid");
+            }
+        }
+    }
+}
