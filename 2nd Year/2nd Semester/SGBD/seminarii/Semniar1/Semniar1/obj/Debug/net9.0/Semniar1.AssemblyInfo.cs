2525f3c9773b8a6c120e482682fd5772741b88de
Emilian Cuciurean <ceir3511@scs.ubbcluj.ro>
2025-03-08 00:02:18 +0200

    ceva

--- 2nd Year/2nd Semester/SGBD/seminarii/Semniar1/Semniar1/obj/Debug/net9.0/Semniar1.AssemblyInfo.cs
+++ 2nd Year/2nd Semester/SGBD/seminarii/Semniar1/Semniar1/obj/Debug/net9.0/Semniar1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semniar1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+712eb5dadd7cec86febd9420444b764b46bd5535")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6561e46197d8cfeda7ea8935a16936e3f3a6a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semniar1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semniar1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
