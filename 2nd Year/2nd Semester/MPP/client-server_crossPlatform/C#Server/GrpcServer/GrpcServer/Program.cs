8b90b8dabc93c5ba8d155e46bc0d0bc3516273c8
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2025-05-02 23:17:55 +0300

    am facut server pentru ceva simplu

---
+++ 2nd Year/2nd Semester/MPP/client-server_crossPlatform/C#Server/GrpcServer/GrpcServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using GrpcServer.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add gRPC services
+builder.Services.AddGrpc();
+builder.Services.AddLogging();
+
+// Configure to listen on HTTP port 5000 only (no HTTPS)
+// builder.WebHost.ConfigureKestrel(options =>
+// {
+//     options.ListenLocalhost(5000); // 👈 important!
+// });
+
+var app = builder.Build();
+
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => $"gRPC server running on port : " + Environment.GetEnvironmentVariable("ASPNETCORE_URLS"));
+
+app.Run();
