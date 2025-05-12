4aa5d164a159a0f3893c358228c64b0006171317
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2025-05-12 10:37:11 +0300

    commit

--- 2nd Year/2nd Semester/MPP/PROIECT-JAVA/Laborator/src/main/java/com/example/laborator/Repository/IParticipantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.laborator.Repository;
-
-import com.example.laborator.Domain.Participant;
-
-public interface IParticipantRepository extends IRepository<Integer, Participant> {
-    /**
-     * some functions for Participant
-     */
-}
