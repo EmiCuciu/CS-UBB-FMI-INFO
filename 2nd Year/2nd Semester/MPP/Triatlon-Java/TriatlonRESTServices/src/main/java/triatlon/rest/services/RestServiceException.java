24e11b8665eb00c73833d952337163d9532f26a4
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2025-08-27 17:59:15 +0300

    am sters ca sa vad daca se renunta la Ruff

--- 2nd Year/2nd Semester/MPP/Triatlon-Java/TriatlonRESTServices/src/main/java/triatlon/rest/services/RestServiceException.java
+++
@@ -1,7 +0,0 @@
-package triatlon.rest.services;
-
-public class RestServiceException extends RuntimeException {
-    public RestServiceException(Exception message) {
-        super(message);
-    }
-}
