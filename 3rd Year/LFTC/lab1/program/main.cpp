14fbe62e7c8daf7c465dd27b7694deb12e0ab163
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2025-10-09 00:56:43 +0300

    commit final

--- 3rd Year/LFTC/lab1/program/main.cpp
+++ 3rd Year/LFTC/lab1/program/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::string nume; // word nume;
-    std::cin >> nume; // hear >> nume;
-    std::cout << nume << std::endl; //  speak << nume << std::endl; | EROARE - in MLP nu exista endl, doar "\n"
+    string nume; // word nume;
+    cin >> nume; // hear >> nume;
+    cout << nume << std::endl; //  speak << nume << std::endl; | EROARE - in MLP nu exista endl, doar "\n"
 
     int a, b;
-    std::cin >> a >> b; // hear >> a >> b;
-    std::cout << a + b; // speak << a + b |  EROARE - in MLP nu se pot executa operatii in instructiunea de iesire ( ex. "a + b")
+    cin >> a >> b; // hear >> a >> b;
+    cout << a + b; // speak << a + b |  EROARE - in MLP nu se pot executa operatii in instructiunea de iesire ( ex. "a + b")
 
     return 0;
 }
