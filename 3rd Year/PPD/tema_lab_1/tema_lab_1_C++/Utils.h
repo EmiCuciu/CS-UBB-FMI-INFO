856409d62f96368fb49d8bb68fe7cb24f9f72744
Emilian Cuciurean <161616826+EmiCuciu@users.noreply.github.com>
2025-10-12 21:11:23 +0300

    gata secvential

---
+++ 3rd Year/PPD/tema_lab_1/tema_lab_1_C++/Utils.h
@@ -0,0 +1,21 @@
+//
+// Created by emicu on 10/12/2025.
+//
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Utils
+{
+public:
+    static void readData(const string& filePath, int& N, int& M, int& n, int& p, vector<vector<int>>& matrix,
+                         vector<vector<int>>& convMatrix);
+    static bool compareFiles(const string& file1, const string& file2);
+};
+
+#endif //UTILS_H
