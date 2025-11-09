0fbdd6ebabc8520087e93d38cc323d050e8581dc
EmiCuciu <emicuciu@gmail.com>
2025-11-10 01:22:42 +0200

    am adaugat gestiunea erorilor pentru Analizatorul Lexical

--- 3rd Year/LFTC/laboratoare/lab1_part3/Lexer.py
+++ 3rd Year/LFTC/laboratoare/lab1_part3/Lexer.py
@@ -1,5 +1,6 @@
-import sys
 import os
+import sys
+
 import token_defs as defs
 from HashSymbolTable import HashSymbolTable
 
@@ -9,6 +10,7 @@ class Lexer:
         self.path = path
         self.ts = HashSymbolTable()
         self.fip = []
+        self.has_errors = False
 
     def classify(self, token: str):
         if token in defs.KEYWORDS:
@@ -30,29 +32,50 @@ class Lexer:
     def tokenize(self):
         try:
             with open(self.path, "r", encoding="utf-8") as f:
-                for line in f:
-                    self._process_line(line.strip())
+                for line_num, line in enumerate(f, 1):
+                    self._process_line(line.strip(), line_num)
         except FileNotFoundError:
             print(f"Eroare: fisierul {self.path} nu exista.")
             sys.exit(1)
 
-    def _process_line(self, line: str):
+        if self.has_errors:
+            print("\n Analiza lexicala s-a incheiat cu erori.", file=sys.stderr)
+
+    def _process_line(self, line: str, line_num: int):
         i = 0
         while i < len(line):
+            start_col = i + 1
             ch = line[i]
+
             if ch.isspace():
                 i += 1
                 continue
+
+            # Gestiune String-uri
             if ch == '"':
                 start = i
                 i += 1
                 while i < len(line) and line[i] != '"':
+                    if line[i] == '\\' and i + 1 < len(line):
+                        i += 2
+                        continue
                     i += 1
-                i += 1
+
+                if i >= len(line) or line[i] != '"':
+                    # Eroare: String neterminat
+                    print(f"Eroare Lexicala [Linia {line_num} , Coloana {start + 1}]: String neterminat.",
+                          file=sys.stderr)
+                    self.has_errors = True
+                    i = len(line)  # oprim procesarea liniei
+                    continue
+
+                i += 1  # trecem peste " de inchidere
                 token = line[start:i]
                 pos = self.ts.insert(token, "CONST_STRING")
                 self.fip.append((defs.ATOM_CODES["CONST_STRING"], pos))
                 continue
+
+            # Gestiune Operatori & Delimitatori
             if i + 1 < len(line) and line[i:i + 2] in defs.MULTI_OPS:
                 op = line[i:i + 2]
                 self.fip.append((defs.ATOM_CODES[op], "-"))
@@ -64,20 +87,35 @@ class Lexer:
                     self.fip.append((code, "-"))
                 i += 1
                 continue
+
+            # Gestiune token-uri generale (ID, CONST, KEYWORD)
             start = i
             while i < len(line) and not line[i].isspace() and line[i] not in defs.OPERATORS and line[i] not in defs.DELIMITERS:
                 i += 1
+
+            # daca nu am avansat inseamna ca ch este un caracter necunoscut
+            if start == i:
+                # Eroare: Simbol necunoscut
+                print(f"Eroare Lexicala [Linia {line_num} , Coloana {start_col}]: Simbol necunoscut.", file=sys.stderr)
+                self.has_errors = True
+                i += 1
+                continue
+
             token = line[start:i]
             ttype = self.classify(token)
+
             if ttype == "KEYWORD":
                 code = defs.ATOM_CODES[token]
                 self.fip.append((code, "-"))
-            elif ttype in ("ID", "CONST_INT", "CONST_FLOAT", "CONST_STRING"):
+            elif ttype in ("ID", "CONST_INT", "CONST_FLOAT"):
                 pos = self.ts.insert(token, ttype)
                 code = defs.ATOM_CODES[ttype]
                 self.fip.append((code, pos))
             else:
-                self.fip.append(("??", "-"))
+                # Eroare: token-urile UNKNOWN (ex: "9abc", "12.3.4")
+                print(
+                    f"Eroare Lexicala [Linia {line_num}, Coloana {start_col}]: Token invalid sau necunoscut: '{token}'", file=sys.stderr)
+                self.has_errors = True
 
     def save(self, out_dir="."):
         ts_path = os.path.join(out_dir, "ts.txt")
--- 3rd Year/LFTC/laboratoare/lab1_part3/main.py
+++ 3rd Year/LFTC/laboratoare/lab1_part3/main.py
@@ -1,7 +1,9 @@
 from Lexer import Lexer
 
 if __name__ == "__main__":
-    INPUT_FILE = "cod_example.txt"
+    # INPUT_FILE = "examples/cod_example.txt"
+    # INPUT_FILE = "examples/cod_example_fara_spatii.txt"
+    INPUT_FILE = "examples/cod_example_ERORI.txt"
     lexer = Lexer(INPUT_FILE)
     lexer.tokenize()
     ts_path, fip_path = lexer.save(".")
